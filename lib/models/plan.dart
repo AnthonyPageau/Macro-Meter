abae2ada4ae6f1baeb0deba321e44de5937ea6a6
Anthony Pageau <antopageau@hotmail.com>
2025-03-20 11:52:36 -0400

    [S2][Refactor]<Plan> Ajouter Getter et Setter à la classe Plan

--- lib/models/plan.dart
+++ lib/models/plan.dart
@@ -2,16 +2,44 @@ import 'package:macro_meter/models/meal.dart';
 
 class Plan {
   Plan(
-      {required this.id,
-      required this.name,
-      required this.date,
+      {required String id,
+      required String name,
+      required DateTime date,
       List<Meal>? meals})
-      : meals = meals ?? [];
+      : _id = id,
+        _name = name,
+        _date = date,
+        _meals = meals ?? [];
 
-  String id;
-  String name;
-  DateTime date;
-  List<Meal> meals;
+  String _id;
+  String _name;
+  DateTime _date;
+  List<Meal> _meals;
+
+  String get id => _id;
+  String get name => _name;
+  DateTime get date => _date;
+  List<Meal> get meals => _meals;
+
+  set id(String value) {
+    if (value.isNotEmpty) {
+      _id = value;
+    }
+  }
+
+  set name(String value) {
+    if (value.isNotEmpty) {
+      _name = value;
+    }
+  }
+
+  set date(DateTime value) {
+    _date = value;
+  }
+
+  set meals(List<Meal> value) {
+    meals = value;
+  }
 
   num totalCalories() {
     num total = 0;
